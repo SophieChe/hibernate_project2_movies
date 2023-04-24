99951e0546e0fa6615477a4d13a4b52b418d106c
SofiyaChe <chebotarevasophie@yandex.ru>
2023-04-24 08:00:15 +0300

    Initial commit

---
+++ src/main/java/com/javarush/Main.java
@@ -0,0 +1,90 @@
+package com.javarush;
+
+import com.javarush.domain.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import com.javarush.dao.ActorDAO;
+import com.javarush.dao.AddressDAO;
+import com.javarush.dao.CategoryDAO;
+import com.javarush.dao.CityDAO;
+import com.javarush.dao.CountryDAO;
+import com.javarush.dao.CustomerDAO;
+import com.javarush.dao.FilmDAO;
+import com.javarush.dao.FilmTextDAO;
+import com.javarush.dao.InventoryDAO;
+import com.javarush.dao.LanguageDAO;
+import com.javarush.dao.PaymentDAO;
+import com.javarush.dao.RentalDAO;
+import com.javarush.dao.StaffDAO;
+import com.javarush.dao.StoreDAO;
+import java.util.Properties;
+
+public class Main {
+    private final SessionFactory sessionFactory;
+
+    private final ActorDAO actorDAO;
+    private final AddressDAO addressDAO;
+    private final CategoryDAO categoryDAO;
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+    private final CustomerDAO customerDAO;
+    private final FilmDAO filmDAO;
+    private final FilmTextDAO filmTextDAO;
+    private final InventoryDAO inventoryDAO;
+    private final LanguageDAO languageDAO;
+    private final PaymentDAO paymentDAO;
+    private final RentalDAO rentalDAO;
+    private final StaffDAO staffDAO;
+    private final StoreDAO storeDAO;
+
+    public Main() {
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/movie");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "$RFVcde3");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Address.class)
+                .addAnnotatedClass(Category.class)
+                .addAnnotatedClass(City.class)
+                .addAnnotatedClass(Country.class)
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Film.class)
+                .addAnnotatedClass(FilmText.class)
+                .addAnnotatedClass(Inventory.class)
+                .addAnnotatedClass(Language.class)
+                .addAnnotatedClass(Payment.class)
+                .addAnnotatedClass(Rental.class)
+                .addAnnotatedClass(Staff.class)
+                .addAnnotatedClass(Store.class)
+                .addProperties(properties)
+                .buildSessionFactory();
+
+        actorDAO = new ActorDAO(sessionFactory);
+        addressDAO = new AddressDAO(sessionFactory);
+        categoryDAO = new CategoryDAO(sessionFactory);
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+        customerDAO = new CustomerDAO(sessionFactory);
+        filmDAO = new FilmDAO(sessionFactory);
+        filmTextDAO = new FilmTextDAO(sessionFactory);
+        inventoryDAO = new InventoryDAO(sessionFactory);
+        languageDAO = new LanguageDAO(sessionFactory);
+        paymentDAO = new PaymentDAO(sessionFactory);
+        rentalDAO = new RentalDAO(sessionFactory);
+        staffDAO = new StaffDAO(sessionFactory);
+        storeDAO = new StoreDAO(sessionFactory);
+
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+}
